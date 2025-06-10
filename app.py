f4f8fc5aee2057198a93bfe04f5282b397603aae
selami538 <erdaltayfun37@gmail.com>
2025-06-10 18:48:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def getstream():
     if param == "getm3u8":
         videoid = request.args.get("videoid")
         veriler = {"AppId": "3", "AppVer": "1025", "VpcVer": "1.0.12", "Language": "tr", "Token": "", "VideoId": videoid}
-        r = requests.post("https://melbet-787587.top/cinema",json=veriler)
+        r = requests.post("https://melbet-017934.top/cinema",json=veriler)
         if "FullscreenAllowed" in r.text:
             veri = r.text
             veri = re.findall('"URL":"(.*?)"',veri)
